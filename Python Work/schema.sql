c4d60db336ebcc28ca063bd2d1c3c34429db60a2
dmiao123 <100433553+dmiao123@users.noreply.github.com>
2022-04-21 21:23:32 +0100

    Add files via upload

--- Python Work/schema.sql
+++ Python Work/schema.sql
@@ -1,9 +1,12 @@
+create schema game_analysis3;
 
-create schema game_analysis;
+drop table if exists game_analysis3.game cascade;
+drop table if exists game_analysis3.developer cascade;
+drop table if exists game_analysis3.user_review_web cascade;
+drop table if exists game_analysis3.youtube cascade;
 
-drop table if exists game_analysis.game cascade;
-create table game_analysis.game(
-    game_id         serial primary key not null,
+create table game_analysis3.game (
+    game_id         serial primary key,
     game_name       varchar(100) not null,
     ranking         int not null,
     release_date    timestamp without time zone not null,
@@ -14,54 +17,48 @@ create table game_analysis.game(
     game_genre      varchar(100) not null,
     game_genre1     varchar(100) not null,
     game_genre2     varchar(100) not null
-    );
+);
 
-drop table if exists game_analysis.developer cascade;
-create table game_analysis.developer(
-    developer_id    serial primary key not null,
-    developer_name  varchar(100) not null,
-    stock_price     float not null
+
+create table game_analysis3.developer (
+    developer_id            serial primary key,
+    game_name               varchar(100) not null,
+    developer_name          varchar(100) not null
+);
+
+create table game_analysis3.game_developer (
+    game_id                 int not null references game_analysis3.game("game_id"),
+    developer_id            int not null references game_analysis3.developer("developer_id")
+);
+
+
+create table game_analysis3.user_review_web (
+    user_review_web_id               serial primary key,
+    game_name                        varchar(100) not null,
+    metascore                        int not null,
+    critic_reviews	                 int not null,
+    user_score                       float,
+    user_score_rating_num            float
+);
+
+create table game_analysis3.game_user_review_web (
+    game_id                 int not null references game_analysis3.game("game_id"),
+    user_review_web_id      int not null references game_analysis3.user_review_web("user_review_web_id")
 );
 
 
 
-drop table if exists game_analysis.game_developer cascade;
-create table game_analysis.game_developer(
-    game_id    int not null references game_analysis.game("game_id"),
-    developer_id    int not null references game_analysis.developer("developer_id")
+create table game_analysis3.youtube (
+    youtube_id       serial primary key,
+    game_name        varchar(100) not null,
+    view_count       int not null,
+    like_count       int not null,
+    favourite_count  int not null,
+    comment_count    int not null
 );
 
-
-
-drop table if exists game_analysis.user_review_web cascade;
-create table game_analysis.user_review_web(
-    game_id                   int not null references game_analysis.game("game_id"),
-    game_name                 varchar(100) not null references game_analysis.game("game_name"),
-    Metascore                 int not null,
-    critic_reviews	          int not null,
-    user_score                int not null,
-    user_score_rating_num     int not null
+create table game_analysis3.game_youtube (
+    game_id                 int not null references game_analysis3.game("game_id"),
+    youtube_id              int not null references game_analysis3.youtube("youtube_id")
 );
-
-
-
-
-drop table if exists game_analysis.youtube cascade;
-create table game_analysis.youtube(
-    game_id    int not null references game_analysis.game("game_id"),
-    game_name varchar(100) not null references game_analysis.game("game_name"),
-    view_count    int not null,
-    like_count    int not null,
-    favourite_count int not null,
-    comment_count   int not null
-);
-
-
-
-
-
-
-
-
-
 
